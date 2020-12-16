53601be71b3a8536963a0c9300184ff3980229cf
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2020-12-16 14:35:37 +0900

    単体テストコードの修正、createメソッドの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it '販売価格が半角英数字の場合、保存できる' do
-        @item.price = '1200'
+        @item.price = 1200
         expect(@item).to be_valid
       end
       it '販売価格が300円以上9,999,999円以下の場合、保存できる' do
-        @item.price = '300'
+        @item.price = 300
         expect(@item).to be_valid
       end
       it '画像が入っていると保存できる' do
@@ -40,27 +40,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Item explanation can't be blank")
       end
       it '商品詳細（カテゴリー）を選択していないと保存できない' do
-        @item.item_category_id = '1'
+        @item.item_category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Item category must be other than 1')
       end
       it '商品詳細（商品の状態）を選択していないと保存できない' do
-        @item.item_status_id = '1'
+        @item.item_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Item status must be other than 1')
       end
       it '配送料の負担が選択されていないと保存できない' do
-        @item.delivery_fee_id = '1'
+        @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
       end
       it '発送元の地域が選択されていないと保存できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it '発送までの日数が選択されていないと保存できない' do
-        @item.delivery_day_id = '1'
+        @item.delivery_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery day must be other than 1')
       end
@@ -75,12 +75,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it '販売価格が300円以上でないと保存できない' do
-        @item.price = '200'
+        @item.price = 200
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '販売価格が9,999,999円以下でないと保存できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
