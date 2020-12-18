dd8671925afcf38c5472450e86ba93901ff092ff
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2020-12-18 20:46:16 +0900

    商品出品を一覧で表示（最新順）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze
 
-  validates :image, :item_name, :item_explanation, :price, presence: true
+  validates :image, :item_name, :item_explanation, :price, :user_id, presence: true
   validates :item_name, length: { maximum: 40 }
   validates :item_explanation, length: { maximum: 1000 }
   validates :item_category_id,
@@ -9,7 +9,6 @@ class Item < ApplicationRecord
             :delivery_fee_id,
             :prefecture_id,
             :delivery_day_id,
-            :user_id,
             presence: true,
             numericality: { other_than: 1 }
   validates :price,
