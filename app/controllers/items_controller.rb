5b0b2906fc9a1b0ba93aa76330a5731b988db1f1
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2020-12-20 19:49:35 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -31,6 +33,7 @@ class ItemsController < ApplicationController
     if item.valid?
       redirect_to item_path
     else
+      @item = item
       render :edit
     end
   end
@@ -49,5 +52,11 @@ class ItemsController < ApplicationController
       :price,
       :image
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
