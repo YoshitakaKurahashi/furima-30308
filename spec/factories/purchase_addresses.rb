2259bdcd7d4aa98d5425cdd1559ebf1d5a1988c2
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2020-12-29 10:18:33 +0900

    buildingの値もfactoryで用意

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id { 2 }
     municipalities { '大阪市' }
     address { '北区梅田１−１' }
+    building { '難波ビル101' }
     telephone { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
